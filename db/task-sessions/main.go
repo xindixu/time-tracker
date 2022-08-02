a0306ffb6642bb90c3ce879c2fb61901801af294
Xindi Xu <xindixu0@gmail.com>
2022-08-01 22:13:26 -0400

    log all sessions

--- db/task-sessions/main.go
+++ db/task-sessions/main.go
@@ -1,6 +1,8 @@
-package taskDB
+package taskSessionDB
 
 import (
+	"encoding/json"
+
 	"github.com/boltdb/bolt"
 	m "github.com/xindixu/todo-time-tracker/models"
 )
@@ -12,4 +14,23 @@ func Setup() error {
 	})
 
 	return err
+}
+
+func LogAllTaskSessions() ([]m.TaskSession, error) {
+	var taskSessions []m.TaskSession
+
+	err := m.TTTDB.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(m.TaskSessionBucketName)
+		bucket.ForEach(func(k, v []byte) error {
+			var taskSession m.TaskSession
+			err := json.Unmarshal(v, &taskSession)
+			if err != nil {
+				return err
+			}
+			taskSessions = append(taskSessions, taskSession)
+			return nil
+		})
+		return nil
+	})
+	return taskSessions, err
 }
